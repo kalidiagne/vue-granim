5fbc3645dc4d74fc140726a24771bb6107c91cf3
Kalidou Diagne <kalidou.diagne@link-me.it>
2017-08-04 12:01:51 +0200

    testing reactivity!

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <template>
   <div id="app">
-    <vue-granim id="my-id-canvas"></vue-granim>
+    <h1>direction: {{ direction }} </h1>
+    <vue-granim id="my-id-canvas" :direction="direction" :opacity="opacity" :isPausedWhenNotInView="false" :states="states"></vue-granim>
+    <div>
+      <button @click="direction = 'radial'">radial</button>
+      <button @click="direction = 'left-right'">left-right</button>
+      <button @click="direction = 'diagonal'">diagonal</button>
+    </div>
+
+    <vue-granim id="test-2" :direction="direction2" :opacity="opacity" :states="states"></vue-granim>
+    <div>
+      <button @click="direction2 = 'radial'">radial</button>
+      <button @click="direction2 = 'left-right'">left-right</button>
+      <button @click="direction2 = 'diagonal'">diagonal</button>
+    </div>
   </div>
 </template>
 
@@ -12,6 +25,23 @@ export default {
   components: {
     Hello,
     VueGranim
+  },
+  data () {
+    return {
+      direction: 'top-bottom',
+      direction2: 'radial',
+      opacity: [0.4, 1],
+      states: {
+        'default-state': {
+          gradients: [
+            ['#fff', '#000'],
+            ['#000', '#fff']
+          ]
+        }
+      }
+    }
+  },
+  created () {
   }
 }
 </script>
--- src/components/VueGranim.vue
+++ src/components/VueGranim.vue
@@ -1,46 +1,69 @@
 <template>
-  <canvas :id="id" :name="name"></canvas>
+  <canvas :id="id"></canvas>
 </template>
-
 <script>
   /* eslint-disable */
+  import Vue from 'vue'
   import Granim from './granim/Granim'
   export default {
-    name: 'vue-granim',
     props: {
       id: String,
       name: String,
+      opacity: Array,
+      states: Object,
+      isPausedWhenNotInView: {
+        type: Boolean,
+        default: true
+      },
+      transitionSpeed: {
+        type: Number,
+        default: 5000
+      },
       direction: {
         type: String,
         default: 'diagonal'
-      },
-      opacity: {
-        type: Array,
-        default: [1, 1]
       }
     },
     data () {
-      return {}
-    },
-    computed: {
-      canvasName () {
-        return this.name
+      return {
+        VueGranim: ''
       }
     },
+    computed: {
+      getOpacity () {
+        let opacity = []
+        let defaultStates =  this.states['default-state'].gradients[0]
+        this.defaultStates.foreach(state => {
+          opacity.push(.5)
+        })
+      }
+    },
+    beforeCreate () {
+    },
     mounted () {
-      var VueGranim = new Granim({ // eslint-disable-line
-        element: document.getElementById(this.id),
+      let self = this
+      self.VueGranim = new Granim({ // eslint-disable-line
+        element: document.getElementById(self.id),
         name: 'granim',
-        opacity: [1, 1],
-        states: {
-          'default-state': {
-            gradients: [
-              ['#834D9B', '#D04ED6'],
-              ['#1CD8D2', '#93EDC7']
-           ]
-          }
-        }
+        opacity: self.opacity,
+        isPausedWhenNotInView: self.isPausedWhenNotInView,
+        transitionSpeed: self.transitionSpeed,
+        states: self.states
       })
+
+      if (self.direction) {
+        self.VueGranim.direction = self.direction
+      }
+    },
+    methods: {
+      setDirection () {
+      }
+    },
+    watch: {
+      direction (val, oldValue) {
+        var self = this
+        self.VueGranim.direction = val
+      }
     }
   }
 </script>
